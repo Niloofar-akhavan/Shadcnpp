11c50b67b4e36670511121252c804301d34972f0
niloofar <niloofar.akhavan.javan@gmail.com>
2026-03-25 16:38:46 +0330

    Add Card component

---
+++ components/ui/card.tsx
@@ -0,0 +1,12 @@
+// Card component
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div className="card">
+      {/* Card content goes here */}
+    </div>
+  );
+};
+
+export default Card;
